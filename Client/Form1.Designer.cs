4cdf33de0b4da3a18f270fc214d0861ab5999017
Steve Patches <steve@careevolution.com>
2015-01-30 20:43:27 -0500

    Padding = none example

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -39,90 +39,97 @@
 			this.btnGenerate = new System.Windows.Forms.Button();
 			this.txtBlockLabelsEnc = new System.Windows.Forms.TextBox();
 			this.txtBlockLabelsPln = new System.Windows.Forms.TextBox();
+			this.txtDecrypted = new System.Windows.Forms.TextBox();
+			this.btnDecrypt = new System.Windows.Forms.Button();
+			this.txtException = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// txtAccountNumber
 			// 
-			this.txtAccountNumber.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtAccountNumber.Location = new System.Drawing.Point(185, 16);
-			this.txtAccountNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtAccountNumber.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtAccountNumber.Location = new System.Drawing.Point(233, 20);
+			this.txtAccountNumber.Margin = new System.Windows.Forms.Padding(4);
 			this.txtAccountNumber.Name = "txtAccountNumber";
-			this.txtAccountNumber.Size = new System.Drawing.Size(268, 22);
+			this.txtAccountNumber.Size = new System.Drawing.Size(268, 29);
 			this.txtAccountNumber.TabIndex = 0;
 			this.txtAccountNumber.Text = "10154265843";
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.Location = new System.Drawing.Point(61, 20);
 			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(120, 16);
+			this.label1.Size = new System.Drawing.Size(164, 21);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Account Number";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label2.Location = new System.Drawing.Point(61, 52);
 			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(96, 16);
+			this.label2.Size = new System.Drawing.Size(131, 21);
 			this.label2.TabIndex = 3;
 			this.label2.Text = "Reason Code";
 			// 
 			// txtReasonCode
 			// 
-			this.txtReasonCode.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtReasonCode.Location = new System.Drawing.Point(185, 48);
-			this.txtReasonCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtReasonCode.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtReasonCode.Location = new System.Drawing.Point(233, 52);
+			this.txtReasonCode.Margin = new System.Windows.Forms.Padding(4);
 			this.txtReasonCode.Name = "txtReasonCode";
-			this.txtReasonCode.Size = new System.Drawing.Size(268, 22);
+			this.txtReasonCode.Size = new System.Drawing.Size(268, 29);
 			this.txtReasonCode.TabIndex = 2;
 			this.txtReasonCode.Text = "ACPT";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label3.Location = new System.Drawing.Point(61, 84);
 			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(56, 16);
+			this.label3.Size = new System.Drawing.Size(76, 21);
 			this.label3.TabIndex = 5;
 			this.label3.Text = "Amount";
 			// 
 			// txtAmount
 			// 
-			this.txtAmount.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtAmount.Location = new System.Drawing.Point(185, 80);
-			this.txtAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtAmount.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtAmount.Location = new System.Drawing.Point(233, 84);
+			this.txtAmount.Margin = new System.Windows.Forms.Padding(4);
 			this.txtAmount.Name = "txtAmount";
-			this.txtAmount.Size = new System.Drawing.Size(268, 22);
+			this.txtAmount.Size = new System.Drawing.Size(268, 29);
 			this.txtAmount.TabIndex = 4;
 			this.txtAmount.Text = "95.99";
 			// 
 			// txtPlainToken
 			// 
-			this.txtPlainToken.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtPlainToken.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.txtPlainToken.Location = new System.Drawing.Point(16, 185);
-			this.txtPlainToken.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtPlainToken.Margin = new System.Windows.Forms.Padding(4);
 			this.txtPlainToken.Name = "txtPlainToken";
-			this.txtPlainToken.Size = new System.Drawing.Size(680, 22);
+			this.txtPlainToken.Size = new System.Drawing.Size(1488, 29);
 			this.txtPlainToken.TabIndex = 6;
 			// 
 			// txtEncryptedToken
 			// 
-			this.txtEncryptedToken.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtEncryptedToken.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.txtEncryptedToken.Location = new System.Drawing.Point(16, 274);
-			this.txtEncryptedToken.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtEncryptedToken.Margin = new System.Windows.Forms.Padding(4);
 			this.txtEncryptedToken.Name = "txtEncryptedToken";
-			this.txtEncryptedToken.Size = new System.Drawing.Size(1131, 22);
+			this.txtEncryptedToken.Size = new System.Drawing.Size(1488, 29);
 			this.txtEncryptedToken.TabIndex = 7;
 			// 
 			// btnGenerate
 			// 
-			this.btnGenerate.Location = new System.Drawing.Point(264, 112);
-			this.btnGenerate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnGenerate.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnGenerate.Location = new System.Drawing.Point(264, 121);
+			this.btnGenerate.Margin = new System.Windows.Forms.Padding(4);
 			this.btnGenerate.Name = "btnGenerate";
 			this.btnGenerate.Size = new System.Drawing.Size(191, 39);
 			this.btnGenerate.TabIndex = 8;
@@ -134,29 +141,63 @@
 			// 
 			this.txtBlockLabelsEnc.BackColor = System.Drawing.SystemColors.Control;
 			this.txtBlockLabelsEnc.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.txtBlockLabelsEnc.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtBlockLabelsEnc.Location = new System.Drawing.Point(18, 294);
+			this.txtBlockLabelsEnc.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtBlockLabelsEnc.Location = new System.Drawing.Point(18, 298);
 			this.txtBlockLabelsEnc.Margin = new System.Windows.Forms.Padding(4);
 			this.txtBlockLabelsEnc.Name = "txtBlockLabelsEnc";
-			this.txtBlockLabelsEnc.Size = new System.Drawing.Size(1131, 15);
+			this.txtBlockLabelsEnc.Size = new System.Drawing.Size(1486, 22);
 			this.txtBlockLabelsEnc.TabIndex = 9;
 			// 
 			// txtBlockLabelsPln
 			// 
 			this.txtBlockLabelsPln.BackColor = System.Drawing.SystemColors.Control;
 			this.txtBlockLabelsPln.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.txtBlockLabelsPln.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtBlockLabelsPln.Location = new System.Drawing.Point(18, 206);
+			this.txtBlockLabelsPln.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtBlockLabelsPln.Location = new System.Drawing.Point(18, 211);
 			this.txtBlockLabelsPln.Margin = new System.Windows.Forms.Padding(4);
 			this.txtBlockLabelsPln.Name = "txtBlockLabelsPln";
-			this.txtBlockLabelsPln.Size = new System.Drawing.Size(680, 15);
+			this.txtBlockLabelsPln.Size = new System.Drawing.Size(680, 22);
 			this.txtBlockLabelsPln.TabIndex = 10;
+			// 
+			// txtDecrypted
+			// 
+			this.txtDecrypted.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtDecrypted.Location = new System.Drawing.Point(18, 375);
+			this.txtDecrypted.Margin = new System.Windows.Forms.Padding(4);
+			this.txtDecrypted.Name = "txtDecrypted";
+			this.txtDecrypted.Size = new System.Drawing.Size(1486, 29);
+			this.txtDecrypted.TabIndex = 11;
+			// 
+			// btnDecrypt
+			// 
+			this.btnDecrypt.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDecrypt.Location = new System.Drawing.Point(264, 328);
+			this.btnDecrypt.Margin = new System.Windows.Forms.Padding(4);
+			this.btnDecrypt.Name = "btnDecrypt";
+			this.btnDecrypt.Size = new System.Drawing.Size(191, 39);
+			this.btnDecrypt.TabIndex = 12;
+			this.btnDecrypt.Text = "Decrypt";
+			this.btnDecrypt.UseVisualStyleBackColor = true;
+			this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+			// 
+			// txtException
+			// 
+			this.txtException.BackColor = System.Drawing.SystemColors.Control;
+			this.txtException.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtException.Location = new System.Drawing.Point(18, 415);
+			this.txtException.Multiline = true;
+			this.txtException.Name = "txtException";
+			this.txtException.Size = new System.Drawing.Size(1129, 236);
+			this.txtException.TabIndex = 13;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1437, 772);
+			this.ClientSize = new System.Drawing.Size(1517, 668);
+			this.Controls.Add(this.txtException);
+			this.Controls.Add(this.btnDecrypt);
+			this.Controls.Add(this.txtDecrypted);
 			this.Controls.Add(this.txtBlockLabelsPln);
 			this.Controls.Add(this.txtBlockLabelsEnc);
 			this.Controls.Add(this.btnGenerate);
@@ -169,7 +210,7 @@
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.txtAccountNumber);
 			this.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -190,6 +231,9 @@
 		private System.Windows.Forms.Button btnGenerate;
 		private System.Windows.Forms.TextBox txtBlockLabelsEnc;
 		private System.Windows.Forms.TextBox txtBlockLabelsPln;
+		private System.Windows.Forms.TextBox txtDecrypted;
+		private System.Windows.Forms.Button btnDecrypt;
+		private System.Windows.Forms.TextBox txtException;
 	}
 }
 
--- Client/Form1.cs
+++ Client/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 using Utils;
 
@@ -6,17 +8,16 @@ namespace Client
 {
 	public partial class Form1 : Form
 	{
-		
 		public Form1()
 		{
 			InitializeComponent();
+			encryptionKey = QuickCrypto.StringToByteArray( this.encryptionKeyString );
 		}
+		private string encryptionKeyString = "4f256766a4075027695bd48b444e1ac5c7c17385a1b4f1779e6385da24d97997";
+		private byte[] encryptionKey;
 
 		private void btnGenerate_Click( object sender, EventArgs e )
 		{
-			var encryptionKeyString = "4f256766a4075027695bd48b444e1ac5c7c17385a1b4f1779e6385da24d97997";
-			byte[] encryptionKey = QuickCrypto.StringToByteArray( encryptionKeyString );
-
 			var expiration = DateTime.UtcNow.AddMinutes( 30 ).ToString( "yyyy-MM-dd HH:mm:ss" );
 			txtPlainToken.Text = txtAccountNumber.Text
 				+ "|" + txtReasonCode.Text
@@ -24,12 +25,14 @@ namespace Client
 				+ "|" + expiration;
 
 			//TODO: need some crypto
-			
+
 			setBlockLabels();
 		}
 
 		private void setBlockLabels()
 		{
+			txtDecrypted.Text = null;
+			txtException.Text = null;
 			txtBlockLabelsEnc.Text = null;
 			for ( int x = 1; x <= txtEncryptedToken.Text.Length / 32; x++ )
 			{
@@ -39,6 +42,52 @@ namespace Client
 			for ( int x = 1; x <= txtPlainToken.Text.Length / 16 + 1; x++ )
 			{
 				txtBlockLabelsPln.Text += "Block " + x.ToString() + "         ";
+			}
+		}
+
+		private void btnDecrypt_Click( object sender, EventArgs e )
+		{
+			txtException.Text = null;
+			txtDecrypted.Text = null;
+			try
+			{
+				byte[] cipherText = QuickCrypto.StringToByteArray( txtEncryptedToken.Text.Substring( 32 ) );
+				byte[] ivBytes = QuickCrypto.StringToByteArray( txtEncryptedToken.Text.Substring( 0, 32 ) );
+
+				string plaintext = null;
+
+				using ( Aes aesAlg = Aes.Create() )
+				{
+					aesAlg.Key = encryptionKey;
+					aesAlg.IV = ivBytes;
+					aesAlg.Padding = PaddingMode.None;
+
+					ICryptoTransform decryptor = aesAlg.CreateDecryptor( aesAlg.Key, aesAlg.IV );
+
+					using ( MemoryStream msDecrypt = new MemoryStream( cipherText ) )
+					using ( CryptoStream csDecrypt = new CryptoStream( msDecrypt, decryptor, CryptoStreamMode.Read ) )
+					using ( StreamReader srDecrypt = new StreamReader( csDecrypt ) )
+					{
+
+						plaintext = srDecrypt.ReadToEnd();
+					}
+				}
+
+				for(int i = 0; i < plaintext.Length; i++) {
+					char c = plaintext[ i ];
+					if ( (int)c < 32 || (int)c > 126 )
+					{
+						txtDecrypted.Text += @"\" + (int)c;
+					}
+					else
+					{
+						txtDecrypted.Text += c;
+					}
+				}
+			}
+			catch ( Exception ex )
+			{
+				txtException.Text = ex.ToString();
 			}
 		}
 	}
