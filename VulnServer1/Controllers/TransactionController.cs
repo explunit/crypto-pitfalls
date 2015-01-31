f1fa5e7c20a55a77ce5be9680a13585b4a47a340
Steve Patches <steve@careevolution.com>
2015-01-31 07:48:20 -0500

    Clean up server code

--- VulnServer1/Controllers/TransactionController.cs
+++ VulnServer1/Controllers/TransactionController.cs
@@ -24,21 +24,23 @@ namespace VulnServer1.Controllers
 			var encryptionKeyString = "4f256766a4075027695bd48b444e1ac5c7c17385a1b4f1779e6385da24d97997";
 			byte[] encryptionKey = QuickCrypto.StringToByteArray( encryptionKeyString );
 
-			//var testValue = "15608714654|ACPT|109.76|20150203 23:59";
-
-			//var testToken = "cc50cf4e7de93ba5e9b15993b0fc1798208d3c91e0c4c73c1b183e8d1fee07e862c6eae43a91d6c7982163dbfc6df64fe322c63a73f7dfdcbe64992ba826dff5a4ccbb57fcc4e701db12016a17b18874";
-
-			//byte[] IV = QuickCrypto.GetRandomIV();
-			//byte[] encrypted = QuickCrypto.EncryptStringToBytes_Aes( testValue, encryptionKey, IV );
-
-			//string ivHex = QuickCrypto.ByteArrayToString( IV );
-			//string tokenHex = QuickCrypto.ByteArrayToString( encrypted );
-
 			byte[] ticketBytes = QuickCrypto.StringToByteArray( ticket.Substring( 32 ) );
 			byte[] ivBytes = QuickCrypto.StringToByteArray( ticket.Substring( 0, 32 ) );
-			string decrypted = QuickCrypto.DecryptStringFromBytes_Aes( ticketBytes, encryptionKey, ivBytes );
+			string decrypted;
+			try
+			{
+				decrypted = QuickCrypto.DecryptStringFromBytes_Aes( ticketBytes, encryptionKey, ivBytes );
+			}
+			catch
+			{
+				return new TransactionResult() { Status = "Bad Token" };
+			}
 
-			if ( decrypted.Split( '|' ).Length == 4 )
+			long accountNumber = 0;
+			string[] ticketParts = decrypted.Split( '|' );
+			// some fake validation
+			bool validAccount = Int64.TryParse( ticketParts[ 0 ], out accountNumber );
+			if ( ticketParts.Length == 4 && validAccount )
 			{
 				return new TransactionResult() { Status = "OK", ConfirmationNumber = 123 };
 			}
